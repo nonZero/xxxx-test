318d58b85fa0f8d101e72364296d9dc5875cf0f6
Udi Oron <udioron@gmail.com>
2020-02-26 10:17:01 +0200

    test

---
+++ precache-manifest.91b183feede055eff0277a9f1a9b2a39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed56822da1cae6b4927bb903f35d820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5582e4f0cc09e3b904c4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d5ccac8229655f83c0a0",
+    "url": "/static/css/main.363cfe61.chunk.css"
+  },
+  {
+    "revision": "5582e4f0cc09e3b904c4",
+    "url": "/static/js/2.6fb028c1.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.6fb028c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ccac8229655f83c0a0",
+    "url": "/static/js/main.79ffae89.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  },
+  {
+    "revision": "e6c35d31bece204e4bb8cf9e51857b3a",
+    "url": "/static/media/jeremy.e6c35d31.gif"
+  },
+  {
+    "revision": "3b4e5c2e938b25f84dd69b7b32a112d2",
+    "url": "/static/media/joey.3b4e5c2e.gif"
+  },
+  {
+    "revision": "90c33c0b1f737e4c6216faae2e61ac08",
+    "url": "/static/media/minions.90c33c0b.gif"
+  },
+  {
+    "revision": "2e35d32a0e5a61adbeabd07e4775b97e",
+    "url": "/static/media/teacher.2e35d32a.gif"
+  }
+]);
